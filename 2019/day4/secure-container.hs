14045b566f31379c07a323b66f0906b70aafb3ac
tr3nx <0wnedbytr3nx@gmail.com>
2019-12-16 12:27:40 -0700

    Day 4, part two

--- 2019/day4/secure-container.hs
+++ 2019/day4/secure-container.hs
@@ -1,27 +1,26 @@
 --- Day 4: Secure Container ---
 
-import Data.List
+import Data.List (group)
 
 -- part one
-hasDoubles :: [Char] -> Bool
-hasDoubles xs = (length
- $ head
- $ sortBy (\x y -> compare (length y) (length x))
- $ groupBy (==) xs) > 1
-
 hasIncreasing :: [Char] -> [Bool]
 hasIncreasing []     = []
 hasIncreasing (x:[]) = [True]
 hasIncreasing (x:xs) = [fromEnum x <= (fromEnum $ head xs)] ++ hasIncreasing xs
 
-solve :: String -> Int
-solve s = length $ [n | n <- [low .. high]
-                      , hasDoubles (show n)
-                      , and $ hasIncreasing (show n)]
+-- part two
+doublesNotLargerGroup :: [Char] -> Bool
+doublesNotLargerGroup xs = any (\x -> (length x) == 2) $ group xs
+
+solve :: String -> String
+solve s = show $ length $
+  [n | n <- map show [low .. high]
+  , doublesNotLargerGroup n
+  , and $ hasIncreasing n]
  where
   low  = (read $ take 6 s) :: Int
   high = (read $ drop 7 s) :: Int
 
 main = do
  let inputs = "234208-765869"
- putStrLn $ "Answer: " ++ show (solve inputs)
+ putStrLn $ "Answer: " ++ solve inputs
